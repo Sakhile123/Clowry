9d574789eb7a9b7e77191429850b6f841ecd4106
Sakhile123 <115566123+Sakhile123@users.noreply.github.com>
2023-10-07 16:17:33 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,58 @@
-// Get all the navigation items
-const navItems = document.querySelectorAll('nav ul li');
 
-// Loop through each item and attach a click event listener
-navItems.forEach((item) => {
-  item.addEventListener('click', () => {
-    // Remove the active class from all items
-    navItems.forEach((item) => {
-      item.classList.remove('active');
+  
+      document.addEventListener("DOMContentLoaded", function () {
+        const profileForm = document.getElementById("profile-form");
+        const editButton = document.getElementById("edit-button");
+        const nameInput = document.getElementById("name");
+        const emailInput = document.getElementById("email");
+        const bioTextarea = document.getElementById("bio");
+    
+        // Disable form inputs initially
+        nameInput.disabled = true;
+        emailInput.disabled = true;
+        bioTextarea.disabled = true;
+    
+        // Function to enable/disable form inputs
+        function toggleFormInputs(enabled) {
+            nameInput.disabled = !enabled;
+            emailInput.disabled = !enabled;
+            bioTextarea.disabled = !enabled;
+        }
+    
+        // Handle Edit Profile button click
+        editButton.addEventListener("click", function () {
+            toggleFormInputs(true);
+            editButton.style.display = "none";
+        });
+    
+        // Handle form submission
+        profileForm.addEventListener("submit", function (event) {
+            event.preventDefault();
+            // Save profile data (you can implement this part with AJAX to a server)
+            // For this example, we will just disable the form inputs and show the Edit button again
+            toggleFormInputs(false);
+            editButton.style.display = "block";
+        });
     });
-
-    // Add the active class to the clicked item
-    item.classList.add('active');
+    
+    document.addEventListener("DOMContentLoaded", function () {
+      const imageUpload = document.getElementById("image-upload");
+      const profileImage = document.getElementById("profile-image");
+  
+      // Handle file input change event
+      imageUpload.addEventListener("change", function () {
+          const file = imageUpload.files[0];
+          if (file) {
+              const reader = new FileReader();
+  
+              reader.onload = function (e) {
+                  profileImage.src = e.target.result;
+              };
+  
+              reader.readAsDataURL(file);
+          }
+      });
   });
-});
-
-nav ul li {
-        transition: background-color 0.3s ease;
-      }
-
-
-
+  
 
 
